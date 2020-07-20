edbb980caca3ac116be127dae1c79b1d426ac23b
jetstreet <jetstreet777@gmail.com>
2020-07-20 11:51:11 +0200

    Initialisation basic react with parcel

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Test() {
+
+  
+  return <div>
+    Hello michel
+  </div>
+}
+
+ReactDOM.render(<Test />, document.getElementById('app'));
